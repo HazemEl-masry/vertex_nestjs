b9cd41a84578a24f7973554d00698ac52d9ce872
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2026-03-08 20:28:29 +0200

    feat: add class-transformer and class-validator dependencies; implement LoginAuthDto with validation

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,1 +1,19 @@
-export class LoginAuthDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class LoginAuthDto {
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  password!: string;
+}
