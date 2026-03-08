a1da4a94d41e27c0de082e54954e95251b7185b3
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2026-03-08 22:33:19 +0200

    feat: fix duplicate import of PrismaModule and create PrismaModule with PrismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [AuthModule, PrismaModule],
