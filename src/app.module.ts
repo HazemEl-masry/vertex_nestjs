67c4e2b778ef55784fb62a1e1e0d8033980424f5
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2026-03-09 17:30:46 +0200

    refactor: remove duplicate import of PrismaModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 
