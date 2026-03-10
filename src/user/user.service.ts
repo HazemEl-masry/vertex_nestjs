ad1c5dadcb6f9c39dd03bd440f416ff250761828
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2026-03-10 21:33:52 +0200

    feat: refactor user service methods and enhance password change functionality

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,19 +2,28 @@ import {
   ConflictException,
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { ChangePasswordUserDto } from './dto/change-password-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
-  /*
-    update user info (username / email)
-  */
+
+  // ==============> Update User Service <==============
+
   async updateUser(updateUserDto: UpdateUserDto, userId: string) {
+    /*
+        fiend user by id
+    */
     const user = await this.prisma.user.findUnique({ where: { id: userId } });
 
+    /*
+        check if the user correct
+    */
     if (!user) {
       throw new NotFoundException('user not found');
     }
@@ -25,6 +34,35 @@ export class UserService {
         username: updateUserDto.username,
         email: updateUserDto.email,
       },
+    });
+  }
+
+  // ==============> Update User Service <==============
+
+  async changePassword(
+    changePasswordUserDto: ChangePasswordUserDto,
+    userId: string,
+  ) {
+    const user = await this.prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    const isMatch = await bcrypt.compare(
+      changePasswordUserDto.currentPassword,
+      user.password,
+    );
+
+    if (!isMatch)
+      throw new UnauthorizedException('Current password is incorrect');
+
+    const hashedPassword = await bcrypt.hash(
+      changePasswordUserDto.newPassword,
+      12,
+    );
+
+    return await this.prisma.user.update({
+      where: { id: userId },
+      data: { password: hashedPassword },
     });
   }
 }
