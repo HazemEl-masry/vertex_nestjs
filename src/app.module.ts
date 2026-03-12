25017e0346d3e40d8b630326e4e1d8e623e3bfb0
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2026-03-12 16:43:14 +0200

    feat: add global configuration module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
